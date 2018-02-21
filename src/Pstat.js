ccdab1bf075fe5d25136f1850d5c6c93f0337ee1
DooSol <doosol@DooSols-MacBook-Pro.local>
2018-02-20 21:40:09 -0600

    Displaying JSON import file done

--- src/Pstat.js
+++ src/Pstat.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Pdata from '../python/data/test_180220.json'
+
 const Stat = () => (
   <div>
     <h1>Player stat will be displayed here</h1>
-    <Link to='/'>Back</Link>
+    <h2>Detailed Playstyle Report of {Pdata.name}</h2>
+    <h3>Player Level : {Pdata.summonerLevel}</h3>
+    <Link to='/'>
+      <button type="button">Back</button>
+    </Link>
   </div>
 )
 export default Stat
