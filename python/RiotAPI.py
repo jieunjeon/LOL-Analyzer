21f8ff46ba11e2969e123eca319a0508b80db150
doosol <se7endragon@naver.com>
2018-02-21 20:16:31 -0600

    additional api test added

--- python/RiotAPI.py
+++ python/RiotAPI.py
@@ -16,25 +16,33 @@ class RiotAPI (object) :
                 args[key] = value
 
 
-        response = requests.get(
-            "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/deserteagie?api_key=RGAPI-e6af808d-ed47-4403-b09d-43dadba22b80",
+        recentMatches = requests.get(
+            "https://na1.api.riotgames.com//lol/match/v3/matchlists/by-account/37040458/recent?api_key="+args,
             params = args
         )
 
-        #print response.URL
-        print(response.json())
+        summonerProf = requests.get(
+            "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/deserteagie?api_key="+args,
+            params = args
+        )
 
-        check = response.json()
+        #print recentMatches.URL
+        print(recentMatches.json())
+
+        check = recentMatches.json()
+        check2 = summonerProf.json()
         print(check)
 
-        print(type(response))
+        print(type(recentMatches))
         print(type(check))
 
 
 
-        with open('../data/test_180220.json', 'w') as fp:
+        with open('./data/apitest.json', 'w') as fp:
             json.dump(check, fp)
 
+        with open('./data/apitest2.json', 'w') as fp:
+            json.dump(check2, fp)
 
 
     def test (self):
